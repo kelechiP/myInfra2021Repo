2cdc450c00cecdb03b6111b4dada6de7144ddaf8
kelechiP <36459520+kelechiP@users.noreply.github.com>
2024-04-10 18:05:19 -0400

    Update backend.tf dynamodb name

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket = "my-s3-bucket-for-tf-state-2024"
     key = "main"
     region = "us-east-1"
-    dynamodb_table = "my-dynamo-db-table-tf"
+    dynamodb_table = "my-dynamodb-table"
   }
 }
